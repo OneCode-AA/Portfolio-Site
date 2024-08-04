8c36e64bf34f4fede86f8e3630e2dd87b45a88e7
abooabdillaah.mbj <onecode.aa@gmail.com>
2024-08-04 02:02:12 -0500

    first commit - still not finished

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,68 @@
+const themeBtn = document.querySelector(".theme-btn")
+const themeSwitch = document.querySelector(".status-indicator")
+const header = document.querySelector(".header")
+
+
+window.addEventListener("scroll", function () {
+ if (window.scrollY >= 10) {
+    header.classList.add("active")
+ } else {
+    header.classList.remove("active")
+ }
+});
+
+
+const menu = document.querySelector('.menu-btn')
+const navMenu = document.querySelector(".nav")
+
+menu.addEventListener('click', function () {
+    if(navMenu.style.display = "none") {
+        navMenu.style.display = "flex"
+        navMenu.classList.toggle("active")
+    } else {
+         navMenu.style.display = "none"
+    }
+})
+
+const year2 = "2024"
+const name2 = "Aboo Abdillaah"
+
+const yearStamp = document.querySelector(".year-st")
+const nameStamp = document.querySelector(".name-st")
+
+yearStamp.textContent = year2
+nameStamp.textContent = name2
+
+const whoami = "Mohamed Jalloh"
+const title1 = " A Front-End Developer.";
+const title2 = " A Software Engineer by Night.";
+const title3 = " A Web Developer by Day."
+const title4 = " A Big UX/UI Design Lover!"
+
+
+const cardBox = document.querySelector(".hero-text")
+
+var typed = new Typed('#hero-text', {
+    strings: [whoami, title1, title2, title3, title4],
+    typeSpeed: 100,
+    backDelay: 2500,
+    loop: true,
+  loopCount: Infinity,
+  });
+
+
+themeSwitch.addEventListener("click", function() {
+    if(document.body.classList.contains("dark-theme")) {
+        document.body.classList.add("light-theme")
+        document.body.classList.remove("dark-theme")
+        themeSwitch.classList.add("day")
+        themeSwitch.classList.remove("night")
+    } else {
+        document.body.classList.add("dark-theme")
+        document.body.classList.remove("light-theme")
+        themeSwitch.classList.add("night")
+        themeSwitch.classList.remove("day")
+    }
+})
+
+
